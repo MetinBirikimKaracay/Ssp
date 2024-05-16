933d521265988352b4855e553d04ba8477563937
metin <metin.karacay@hotmail.com>
2024-05-16 07:13:43 +0300

    - UI improvement about edit profile

--- app/src/main/java/com/example/ssp/editProfile/EditProfileFragment.kt
+++ app/src/main/java/com/example/ssp/editProfile/EditProfileFragment.kt
@@ -1,15 +1,31 @@
 package com.example.ssp.editProfile
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.ssp.R
 import com.example.ssp.databinding.FragmentEditProfileBinding
 
 class EditProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentEditProfileBinding
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onResume() {
+        super.onResume()
+        val heights: MutableList<String> = mutableListOf()
+        for (i in 140..210) {
+            heights.add(i.toString())
+        }
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.item_height_dropdown, heights)
+        binding.height.setAdapter(arrayAdapter)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +35,33 @@ class EditProfileFragment : Fragment() {
         binding = FragmentEditProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        handleViewOptions()
+        sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
     }
+
+    fun handleViewOptions() {
+        with(binding) {
+            buttonSave.setOnClickListener {
+                saveProfileData()
+            }
+        }
+    }
+    private fun saveProfileData() {
+        val age = binding.editTextAge.text.toString()
+        val weight = binding.editTextWeight.text.toString()
+        val height = binding.height.text.toString()
+
+        val editor = sharedPreferences.edit()
+        editor.putString("age", age)
+        editor.putString("weight", weight)
+        editor.putString("height", height)
+        editor.apply()
+
+        // Kullanıcıya bilginin kaydedildiği bildirimi gösterilebilir
+        Toast.makeText(requireContext(), "Bilgiler kaydedildi.", Toast.LENGTH_SHORT).show()
+    }
+
 }
--- app/src/main/java/com/example/ssp/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/ssp/profile/ProfileFragment.kt
@@ -1,6 +1,9 @@
 package com.example.ssp.profile
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +15,7 @@ import com.example.ssp.databinding.FragmentProfileBinding
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
     private val mainViewModel: MainViewModel by activityViewModels()
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
@@ -21,12 +25,21 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         handleViewOptions()
+
     }
 
     fun handleViewOptions() {
         with(binding) {
             buttonDirection.setOnClickListener {
                 mainViewModel.onEditProfileFragmentButtonClick()
+            }
+            buttonGetProfile.setOnClickListener {
+                sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+                val savedAge = sharedPreferences.getString("age", "")
+                val savedWeight = sharedPreferences.getString("weight", "")
+                val savedHeight = sharedPreferences.getString("height", "")
+
+                Log.i("ProfileFragment", "Saved age: $savedAge, saved weight: $savedWeight, saved height: $savedHeight")
             }
         }
     }
